4646025237cd0d4c25b0539725225c008d390dc2
praveen <praveenyeramala55@gmail.com>
2022-08-31 11:19:12 +0530

    Added extra code

--- Addition.java
+++ Addition.java
@@ -2,15 +2,18 @@ class Addition
 {
 	public static void main(String args[])
 	{
-		int num1,num2,res;
+		int num1,num2,n1,n2,res;
+		n1=25;
 		Scanner s =new Scanner(System.in);
 		System.out.println("Enter num1 value");
                 num1=s.nextInt();
 		System.out.println("Enter num2 value");
                 num2=s.nextInt();
 		res=num1*num2;
+		n2=n1++;
 		System.out.println("The sum of given numbers is "+res);
 		System.out.println("Thank you for joining GCP Devops COurse");
+		System.out.println("n2 value is :"+n2);
 		//This is End of the File
 	}
 }
